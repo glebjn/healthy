0ce82180ebcb4225880aeb271294c73b74be45de
Gleb Kalinichenko <gakalinich@mts.ru>
2023-06-19 20:31:09 +0300

    Add base structure of auth screen

--- app/src/main/java/com/i/healthy/MainActivity.kt
+++ app/src/main/java/com/i/healthy/MainActivity.kt
@@ -3,16 +3,27 @@ package com.i.healthy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.i.auth_impl.AuthScreen
 import com.i.healthy.ui.theme.HealthyTheme
 
+private const val LOGIN_SCREEN_ROUTE = "login_route"
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +33,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController: NavHostController = rememberNavController()
+                    NavHost(
+                        modifier = Modifier,
+                        navController = navController,
+                        startDestination = LOGIN_SCREEN_ROUTE
+                    ) {
+                        composable(LOGIN_SCREEN_ROUTE) {
+                            AuthScreen()
+                        }
+                    }
                 }
             }
         }
@@ -31,10 +51,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- feature/auth/auth-impl/src/main/java/com.i.auth_impl/AuthScreen.kt
+++ feature/auth/auth-impl/src/main/java/com.i.auth_impl/AuthScreen.kt
@@ -1,12 +1,71 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
 package com.i.auth_impl
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun AuthScreen() {
+    Column {
+        Header()
+        LoginInput()
+        Footer()
+    }
+}
 
+@Composable
+fun Header() {
+    val text = stringResource(id = R.string.auth_login_title)
+    Text(text)
+}
+
+@Composable
+fun LoginInput() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    TextField(
+        value = email,
+        onValueChange = { text ->
+            email = text
+        },
+        placeholder = {
+            val hint = stringResource(id = R.string.auth_login_email_hint)
+            Text(hint)
+        }
+    )
+    TextField(
+        value = password,
+        onValueChange = { text ->
+            password = text
+        },
+        placeholder = {
+            val hint = stringResource(id = R.string.auth_login_password_hint)
+            Text(hint)
+        }
+    )
+}
+
+@Composable
+fun Footer() {
+    Button(
+        onClick = {  }
+    ) {
+        val text = stringResource(id = R.string.auth_login_apply)
+        Text(text = text)
+    }
+    Text(text = stringResource(id = R.string.auth_login_restore_password))
 }
 
 @Preview
