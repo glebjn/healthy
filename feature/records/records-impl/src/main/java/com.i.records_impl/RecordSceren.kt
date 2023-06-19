7d4f7ba335d02e8963c12e6d35ef964111892a5c
Gleb Kalinichenko <gakalinich@mts.ru>
2023-06-19 20:37:33 +0300

    Add base templates records screens

---
+++ feature/records/records-impl/src/main/java/com.i.records_impl/RecordSceren.kt
@@ -0,0 +1,18 @@
+package com.i.records_impl
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RecordScreen() {
+
+}
+
+@Preview
+@Composable
+fun RecordScreenPreview() {
+    MaterialTheme {
+        RecordScreen()
+    }
+}
---
+++ feature/records/records-impl/src/main/java/com.i.records_impl/RecordsListScreen.kt
@@ -0,0 +1,18 @@
+package com.i.records_impl
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RecordsListScreen() {
+
+}
+
+@Preview
+@Composable
+fun RecordsListScreenPreview() {
+    MaterialTheme {
+        RecordsListScreen()
+    }
+}
